435749e5696b5ed4326ed0886b4159763bda78de
unknown <gbebejunior@gmail.com>
2022-02-21 03:24:43 -0800

    Working environment and some dependencies

---
+++ index.js
@@ -0,0 +1,56 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const displayRoutes = require('express-routemap')
+const mySqlConnection = require('./config/mysql')
+// const userRoutes = require('./routes/users.routes')
+// const billPaymentRoutes = require('./routes/bills_payments.routes')
+// const paymentRoutes = require('./routes/payment.routes')
+
+const port = process.env.PORT
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+app.listen(port, () => {
+    console.log(`i am listening on ${port}`)
+    displayRoutes(app)
+})
+
+mySqlConnection.connect(err => {
+    if (err) throw err.stack
+    // connected!
+    console.log('successfully connected: ' , mySqlConnection.threadId)
+  })
+
+
+
+app.use(userRoutes)
+// app.use(billPaymentRoutes)
+// app.use(paymentRoutes)
+//app.use(AppRoutes)
+
+
+app.get('/', (req, res) => {
+    
+    res.status(200).send({
+        status: "error",
+        message: "Welcome guys",
+        data: []
+    })
+
+})
+
+/*
+    Error 404
+*/
+app.use((req, res, next) => {
+
+        res.status(404).send({
+            status: "error",
+            message: "Seems you got lost. so sorry"
+        })
+
+})
